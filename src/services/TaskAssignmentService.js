2e786bb12547a81a61d1a9c88d646da13bc93154
Eduardo Herrera <pedro.herrera@becarios.infotec.mx>
2022-10-04 20:44:32 -0500

    changes

--- src/services/TaskAssignmentService.js
+++
@@ -1,6 +0,0 @@
-import { axiosInstance } from 'boot/axios'
-
-export const index = (payload) => {
-  const URL = '/api/task-assignments'
-  return axiosInstance.get(URL, payload).then((res) => res.data.taskAssignments)
-}
